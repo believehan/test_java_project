93bc5c771b42b65f3454ae814fdff6633dad9e67
believehan <90814456+believehan@users.noreply.github.com>
2025-02-21 10:30:13 +0900

    if 공부

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,16 @@
-import java.util.Scanner;
-
 class Main {
-   public static void main(String[] args) {
-      Scanner scanner = new Scanner(System.in);
-      int n1, n2;
-      
-      System.out.print("첫 번재 수 입력 : ");
-      n1=scanner.nextInt();
-      System.out.print("두 번재 수 입력 : ");
-      n2=scanner.nextInt();
-      
-//      System.out.println( n1 == n2 ? "두 수는 같음" : "큰 수는 " + ( (n1 > n2) ? n1 : n2 ) );
-      System.out.printf( n1 == n2 ? "\n두 수는 같음" : "\n큰 수는 %d", n1 > n2 ? n1 : n2); 
-      
-      scanner.close();
-   }
+	public static void main(String[] args) {
+		int age = 25;
+		boolean man = true;
+
+		if (age >= 19) {
+			if (man) {
+				System.out.println("성인 남성 : 25000원");
+			} else {
+				System.out.println("성인 여성 : 21000원");
+			}
+		}else {
+			System.out.println("청소년 : 15000원");
+		}
+	}
 }
