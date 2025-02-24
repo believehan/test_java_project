b93db18f64da2b3a98bad0213797ebbc5c51fbaa
believehan <90814456+believehan@users.noreply.github.com>
2025-02-24 17:57:44 +0900

    배열 공부중

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,66 @@
-import java.util.Scanner;
+class Tank {
+	String name;
+	int cannon;
+	boolean missile;
+	boolean reset = false;
 
-class Main {
-	public static void main(String[] args) {
-		int star;
-		Scanner num = new Scanner(System.in);
+	void setting(String name, int cannon, boolean missile) {
+		this.name = name;
+		this.cannon = cannon;
+		this.missile = missile;
+		reset = true;
+	}
 
-		do {
-			System.out.print("출력할 줄 수 입력 (1보다 큰 자연수만 입력): ");
-			star = num.nextInt();
-		} while (star <= 0);
-
-		for (int i = 1; i <= star; i++) { // 줄수
-
-			for (int j = 0; j < star * 2 - i; j++) { // 열
-				if (j < i - 1) {
-					System.out.print(" ");
-				} else {
-					System.out.print("*");
-				}
-			}
-			System.out.println();
+	void tank_type() {
+		if (reset) {
+			System.out.printf("탱크명 : %s, 포종류 : %d단계, ", name, cannon);
+			System.out.println(missile ? "미사일 장착" : "미사일 미장착");
+//			System.out.printf("미사일 %s장착\n", missile ? "":"미");// 선생님 답
+		} else {
+			System.out.println("탱크 제작 실패");
 		}
+	}
 
-		num.close();
+	void attack() {
+		System.out.println("\n");
+		System.out.printf("< %s 공격>", name);
+		
+		String casound="";
+		switch (cannon) {
+		case 1:
+			casound = "펑";
+			break;
+		case 2:
+			casound = "펑펑";
+			break;
+
+		case 3:
+			casound = "콰광";
+			break;
+		}
+		
+		System.out.printf("대포발사 : %s", casound);
+		if (missile) System.out.println("\n미사일 발사 : 초전박살");
+	}
+
+}
+
+public class Main {
+	public static void main(String[] args) {
+		Tank k1 = new Tank();
+		Tank k2 = new Tank();
+		Tank k3 = new Tank();
+
+		k1.setting("K1", 1, false);
+		k2.setting("K2", 2, false);
+		k3.setting("K3", 3, true);
+
+		k1.tank_type();
+		k2.tank_type();
+		k3.tank_type();
+
+		k1.attack();
+		k2.attack();
+		k3.attack();
 	}
 }
