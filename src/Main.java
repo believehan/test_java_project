db2a4fec90220fa0ba9a0d9db5516f7e85b490dc
believehan <90814456+believehan@users.noreply.github.com>
2025-03-04 11:15:22 +0900

    equals 공부

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,45 @@
-/*
- *  < 인터페이스를 통한 공통기능의 확장 >
- *  개별 객체에 대한 공통적인 기능에 대하여 그 세부적 내용만 차이가 있는 경우, 인터페이스에 그 공통
- *  기능을 명시하고 다수의 클래스에 각각의 세부 내용을 구현 재정의(Overriding)함으로써 개별
- *  객체 생성시 동일 인터페이스 타입 변수로 받음에 따라 공통 기능에 대한 세부적 다형성 확보가능
-*/
-interface Func{		// 공통 인터페이스
-	void func1();
-	void func2();
-}
+class Car {
+	String name;
+	String color;
 
-class ImpleClass1 implements Func {
-	@Override
-	public void func1() {
-		System.out.println("구현1-1");
+	public Car(String name, String color) {
+		this.name = name;
+		this.color = color;
+	}
+}
+class OverridingCar {
+	String name;
+	String color;
+	
+	public OverridingCar(String name, String color) {
+		this.name = name;
+		this.color = color;
 	}
 
 	@Override
-	public void func2() {
-		System.out.println("구현1-2");		
-	}	
-}
-class ImpleClass2 implements Func {
-	@Override
-	public void func1() {
-		System.out.println("구현2-1");
+	public boolean equals(Object arg0) {
+		if (arg0 instanceof OverridingCar) {
+			
+			OverridingCar tObj = (OverridingCar)arg0;
+			return name.equals(tObj.name) && color.equals(tObj.color);
+		}
+		return false;
 	}
 	
-	@Override
-	public void func2() {
-		System.out.println("구현2-2");		
-	}	
+	
 }
 
 public class Main {
 	public static void main(String[] args) {
-		Func t1 = new ImpleClass1();	// 공통 인터페이스 타입변수에 개별 구현 객체를 대입함으로써
-		Func t2 = new ImpleClass2();	// 객체별 공통 기능에 대한 세부적 다형성 확보
+		OverridingCar sonata = new OverridingCar("소나타", "화이트");
+		OverridingCar sonata2 = new OverridingCar("소나타", "화이트");
+		OverridingCar sonata3 = new OverridingCar("스포티지", "화이트");
+		Car sonata4 = new Car("소나타", "화이트");
 
-		t1.func1();
-		t1.func2();
-		t2.func1();
-		t2.func2();
-
+		System.out.printf("sonata와 sonsta2는 %s\n", sonata.equals(sonata2) ? "같다" : "다르다");
+		System.out.printf("sonata2와 sonsta3는 %s\n", sonata2.equals(sonata3) ? "같다" : "다르다");
+		System.out.printf("sonata와 sonsta4는 %s\n", sonata.equals(sonata4) ? "같다" : "다르다");
+		
+		// (41행 주석) sonata4는 22행의 instanceof연산자에 의해 OverridngCar타입의 객체가 아님으로 평가되므로 바로 false리턴.
 	}
 }
