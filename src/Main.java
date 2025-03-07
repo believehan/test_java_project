2873f31166499b88ac214a57edec2de02e8163c3
believehan <90814456+believehan@users.noreply.github.com>
2025-03-07 17:05:35 +0900

    스레드 공부

--- src/Main.java
+++ src/Main.java
@@ -1,143 +1,65 @@
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
-class ClientManager {
-	private Scanner sc;
-	private HashMap<Integer, ClientInfo> client;
-
-	private class ClientInfo {
-		private static int clientCnt = 0;
-		private int clientNum;
-		private String clientName;
-		private int clientAge;
-		private String clientContact;
-
-		public ClientInfo(String clientName, int clientAge, String clientContact) {
-			this.clientName = clientName;
-			this.clientAge = clientAge;
-			this.clientContact = clientContact;
-			clientNum = ++clientCnt;
-		}
-	}
-
-	public ClientManager(int i) {
-		sc = new Scanner(System.in);
-		client = new HashMap<Integer, ClientInfo>(i);
-	}
-
-	public void viewClientInfo() {
-		System.out.println("< 전체 등록 고객 확인 시스템 >\n");
-		System.out.printf("----등록된 전체 고객 수: %d -----\n\n", client.size());
-
-		for (Map.Entry<Integer, ClientInfo> entry : client.entrySet()) {
-			ClientInfo client = entry.getValue();
-
-			System.out.printf("고객번호 : %d\n", entry.getKey());
-			System.out.printf("고객명 : %s\n", client.clientName);
-			System.out.printf("나이 : %d\n", client.clientAge);
-			System.out.printf("연락처 : %s\n\n", client.clientContact);
-		}
-		System.out.println("\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>전체 등록고객 확인완료!!\n\n\n");
-	}
-
-	public void addClient() {
-		System.out.println("< 고객등록 시스템 >\n");
-
-		String name, contact;
-		int age;
-		for (;;) {
-			System.out.print("고객명 : ");
-			name = sc.nextLine();
-			if (name.isEmpty()) {
-				break;
-			} else {
-				System.out.print("나이 : ");
-				age = sc.nextInt();
-				System.out.print("연락처 : ");
-				contact = sc.next();
-				System.out.println();
-				ClientInfo putinfo = new ClientInfo(name, age, contact);
-				client.put(putinfo.clientNum, putinfo);
-
-				sc.nextLine(); // 남아있는 개행문자를 초기화
-			}
-		}
-	}
-
-	public void searchClient() {
-//		int num;
-//		System.out.println("< 등록고객 검색 시스템 >\n");
-//
-//		for (;;) {
-//			System.out.println("\n==========================================\n");
-//			System.out.print("찾을 고객번호 : ");
-//			num = sc.nextInt();
-//			if (num == -1) {
-//				break;
-//			}
-//			if (client.containsKey(num)) {
-//				if (client.get(num).clientNum == num) {
-//					System.out.println("----검색된 고객정보----");
-//					System.out.printf("고객명 : %s\n", client.get(num).clientName);
-//					System.out.printf("나이 : %d\n", client.get(num).clientAge);
-//					System.out.printf("연락처 : %s\n", client.get(num).clientContact);
-//				}
-//			} else {
-//				System.out.println("해당 고객번호는 존재하지 않습니다.\n");
-//			}
-//		}
-//		System.out.println("\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>검색완료!\n\n");
-	}
-
-	public void removeClient() {
-		int num;
-		System.out.println("< 고객등록 해지 시스템 >\n");
-		for (;;) {
-			System.out.print("삭제 할 고객번호:");
-			num = sc.nextInt();
-			if (num == -1) {
-				break;
-			}
-			if (client.containsKey(num)) {
-				no :
-				for (;;) {
-					if (client.get(num).clientNum == num) {
-						String yesno;
-						System.out.printf("정말 고객번호 %d번 %s님을 등록 해지하시겠습니까?(y/n) : ", num, client.get(num).clientName);
-						yesno = sc.next();
-						switch (yesno) {
-						case "y": {
-							System.out.printf("%d번, %s님을 등록 해지하였습니다.\n", num, client.get(num).clientName);
-							client.remove(num);
-						}
-						case "n": {
-							break no;
-						}
-						default:
-							System.out.println("입력이 잘못 되었습니다.\n");
-						}
-					}
-				}
-			} else {
-				System.out.println("해당 고객번호는 존재하지 않습니다.\n");
-			}
-		}
-		System.out.println("\n\n>>>>>>>>>>>>>>>>>>>>>>>>>>>> 삭제 완료!\n\n");
-	}
-
-}
-
 public class Main {
+	public class Custom extends Exception {
+
+	}
+
+	static final int correctpw = 1234; // 저장되어있는 비밀번호로 가정
+	static int pw = 0; // 입력 비밀번호
+	static int fMth = 0; // inputPw메서드에 대한 최초 호출 명시 스위치.
+
 	public static void main(String[] args) {
-		ClientManager clientManager = new ClientManager(10);
+		Scanner sc = new Scanner(System.in);
 
-		clientManager.viewClientInfo(); // 전체 등록고객 확인 메서드.
+		inputPw(sc); // 비밀번호 입력을 위한 일회성 메서드. 메서드 재호출 시 스캐너 객체를 닫어 호출 불가 상태이어야 함으로 설계.
+						// 즉, 비밀번호가 일치하던 불일치하던 3회 입력 후에는 스태너 객체를 close하여 15행의 메서드 호출이
+						// 불가하도록 설계.
 
-		clientManager.addClient(); // 고객등록 메서드.
-		clientManager.searchClient(); // 고객검색 메서드.
-		clientManager.removeClient(); // 고객등록 해지 메서드.
+		inputPw(sc); // 비밀번호 3회 불일치 시 재입력 시도 가정.
+		inputPw(sc); // 비밀번호 3회 불일치 시 재입력 시도 가정.
+		inputPw(sc); // 비밀번호 3회 불일치 시 재입력 시도 가정.
+		inputPw(sc); // 비밀번호 3회 불일치 시 재입력 시도 가정.
+	}
 
-		clientManager.viewClientInfo(); // 전체 등록고객 확인 메서드
+	static void inputPw(Scanner sc) {
+		try {
+			for (int i = 0; i < 3; i++) {
+				try {
+					if (fMth == 0) { // inputPw메서드 최초 호출 시에만
+						System.out.print("패스워드 4자리 숫자 입력 : "); // 23행의 문자열이 출력되도록 설정.
+					}
+
+					pw = sc.nextInt();
+					if (pw == correctpw) {
+						System.out.println("패스워드 일치 로그인 성공");
+
+						return;
+					}
+				} catch (Exception e) {
+					try {
+						sc.nextLine(); // 26행에서 문자 입력 시 예외 발생으로 인한 입력버퍼 개행문자
+
+					} catch (Exception e1) {
+						if (pw != correctpw) {
+							System.out.println("은행가라");
+						}
+						return;
+					}
+					System.out.println(i + 1 + "회 입력 오류"); // 제거. 또한 비밀번호 일치 여부를 떠나 15행의 메서드 재
+					System.out.println("숫자만 입력 가능합니다."); // 호출 시 이미 소멸된 스캐너 객체를 26행에서 불법적 접근을
+					continue; // 하게됨으로써 예외가 발생되어 catch블럭에 의해 처리가 되나
+								// 당행에서 다시 예외가 발생되어 강제종료.
+				}
+
+				System.out.println(i + 1 + "회 입력 오류");
+			}
+			System.out.println("비밀번호 3회 오류 접속 불가");
+		} finally {
+			
+			fMth = 1; // inputPw메서드에 대한 최초 호출이 아님 명시 스위치
+			sc.close(); // 비밀번호 3회 입력 오류 시 스캐너 객체 해제
+		}
+
 	}
 }
